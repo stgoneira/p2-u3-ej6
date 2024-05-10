97ac07900a93644e9dbcebedbb8b42cc9e5f8b96
hcslearning <learning@hardcybersoft.cl>
2024-05-10 11:25:59 -0400

    Pantalla Inicial

--- app/src/main/java/cl/stgoneira/android/iplavisos/MainActivity.kt
+++ app/src/main/java/cl/stgoneira/android/iplavisos/MainActivity.kt
@@ -3,13 +3,20 @@ package cl.stgoneira.android.iplavisos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cl.stgoneira.android.iplavisos.ui.theme.IplavisosTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    IplAvisosUI()
                 }
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun IplAvisosUI() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // TITULO
+        Text(
+            "IplAvisos",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(top = 70.dp)
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IplavisosTheme {
-        Greeting("Android")
+        // BOTONES
+        Column {
+            Button(
+                modifier = Modifier.width(200.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Buscar Avisos")
+            }
+            Button(
+                modifier = Modifier.width(200.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Publicar")
+            }
+        }
+
+        // FOOTER
+        Text(
+            "Todos los derechos reservados 2024",
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.inversePrimary
+        )
     }
 }
--- app/src/main/java/cl/stgoneira/android/iplavisos/ui/theme/Type.kt
+++ app/src/main/java/cl/stgoneira/android/iplavisos/ui/theme/Type.kt
@@ -14,13 +14,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        fontFamily = FontFamily.Monospace,
+        fontWeight = FontWeight.Bold,
+        fontSize = 60.sp,
+        lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
@@ -30,5 +29,4 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
